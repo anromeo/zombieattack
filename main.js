6383f484a63f00dde22c3457e10a975e249d1a1b
Vladimir Smirnov <vsmirnov@D-10-16-26-206.dhcp4.washington.edu>
2016-01-18 13:06:58 -0800

    added assetmanager, gameengine, main and index

---
+++ main.js
@@ -0,0 +1,199 @@
+// function Animation(spriteSheet, startX, startY, frameWidth, frameHeight, frameDuration, frames, loop, reverse) {
+//     this.spriteSheet = spriteSheet;
+//     this.startX = startX;
+//     this.startY = startY;
+//     this.frameWidth = frameWidth;
+//     this.frameDuration = frameDuration;
+//     this.frameHeight = frameHeight;
+//     this.frames = frames;
+//     this.totalTime = frameDuration * frames;
+//     this.elapsedTime = 0;
+//     this.loop = loop;
+//     this.reverse = reverse;
+// }
+
+function Animation(spriteSheet, frameWidth, frameHeight, frameDuration, frames, loop, reverse) {
+    this.spriteSheet = spriteSheet;
+    this.frameWidth = frameWidth;
+    this.frameDuration = frameDuration;
+    this.frameHeight = frameHeight;
+    this.frames = frames;
+    this.totalTime = frameDuration * frames;
+    this.elapsedTime = 0;
+    this.loop = loop;
+    this.reverse = reverse;
+}
+
+// Animation.prototype.drawFrame = function (tick, ctx, x, y, scaleBy) {
+//     var scaleBy = scaleBy || 1;
+//     this.elapsedTime += tick;
+//     if (this.loop) {
+//         if (this.isDone()) {
+//             this.elapsedTime = 0;
+//         }
+//     } else if (this.isDone()) {
+//         return;
+//     }
+//     var index = this.reverse ? this.frames - this.currentFrame() - 1 : this.currentFrame();
+//     var vindex = 0;
+//     if ((index + 1) * this.frameWidth + this.startX > this.spriteSheet.width) {
+//         index -= Math.floor((this.spriteSheet.width - this.startX) / this.frameWidth);
+//         vindex++;
+//     }
+//     while ((index + 1) * this.frameWidth > this.spriteSheet.width) {
+//         index -= Math.floor(this.spriteSheet.width / this.frameWidth);
+//         vindex++;
+//     }
+
+//     var locX = x;
+//     var locY = y;
+//     var offset = vindex === 0 ? this.startX : 0;
+//     ctx.drawImage(this.spriteSheet,
+//                   index * this.frameWidth + offset, vindex * this.frameHeight + this.startY,  // source from sheet
+//                   this.frameWidth, this.frameHeight,
+//                   locX, locY,
+//                   this.frameWidth * scaleBy,
+//                   this.frameHeight * scaleBy);
+// }
+
+Animation.prototype.drawFrame = function (tick, ctx, x, y) {
+    this.elapsedTime += tick;
+    if (this.isDone()) {
+        if (this.loop) this.elapsedTime = 0;
+    }
+    var frame = this.currentFrame();
+    var xindex = 0;
+    var yindex = 0;
+    // if (frame > 13) {
+    //     frame = 26 - frame;
+    // }
+    // xindex = frame % 5;
+       xindex = frame;
+    // yindex = Math.floor(frame / 5);
+
+    console.log(frame + " " + xindex + " " + yindex);
+
+    //this.spriteSheet = this.rotateAndCache((ASSET_MANAGER.getAsset("./img/man.png")), 90);
+
+    ctx.drawImage(this.spriteSheet,
+                 xindex * this.frameWidth, yindex * this.frameHeight,  // source from sheet
+                 this.frameWidth, this.frameHeight,
+                 x, y,
+                 this.frameWidth,
+                 this.frameHeight);
+}
+
+Animation.prototype.currentFrame = function () {
+    return Math.floor(this.elapsedTime / this.frameDuration);
+}
+
+Animation.prototype.isDone = function () {
+    return (this.elapsedTime >= this.totalTime);
+}
+
+function Background(game) {
+    Entity.call(this, game, 0, 400);
+    this.radius = 200;
+}
+
+Background.prototype = new Entity();
+Background.prototype.constructor = Background;
+
+Background.prototype.update = function () {
+}
+
+Background.prototype.draw = function (ctx) {
+    ctx.fillStyle = "SaddleBrown";
+    ctx.fillRect(0,500,800,300);
+    Entity.prototype.draw.call(this);
+}
+
+// function Unicorn(game) {
+//     this.animation = new Animation(ASSET_MANAGER.getAsset("./img/RobotUnicorn.png"), 0, 0, 206, 110, 0.02, 30, true, true);
+//     this.jumpAnimation = new Animation(ASSET_MANAGER.getAsset("./img/RobotUnicorn.png"), 618, 334, 174, 138, 0.02, 40, false, true);
+//     this.jumping = false;
+//     this.radius = 100;
+//     this.ground = 400;
+//     Entity.call(this, game, 0, 400);
+// }
+
+function Man(game, spritesheet) {
+    this.animation = new Animation(spritesheet, 79, 87, 0.05, 6, true, false); 
+    //this.spriteSheet = this.rotateAndCache((ASSET_MANAGER.getAsset("./img/man.png")), 90);
+   // this.animation = new Animation(spritesheet, 80, 80, 0.05, 1, true, false);
+    this.x = 0;
+    this.y = 0;
+    this.game = game;
+    this.ctx = game.ctx;
+}
+
+// Unicorn.prototype = new Entity();
+// Unicorn.prototype.constructor = Unicorn;
+
+Man.prototype = new Entity();
+Man.prototype.constructor = Man;
+
+// Unicorn.prototype.update = function () {
+//     if (this.game.space) this.jumping = true;
+//     if (this.jumping) {
+//         if (this.jumpAnimation.isDone()) {
+//             this.jumpAnimation.elapsedTime = 0;
+//             this.jumping = false;
+//         }
+//         var jumpDistance = this.jumpAnimation.elapsedTime / this.jumpAnimation.totalTime;
+//         var totalHeight = 200;
+
+//         if (jumpDistance > 0.5)
+//             jumpDistance = 1 - jumpDistance;
+
+//         //var height = jumpDistance * 2 * totalHeight;
+//         var height = totalHeight*(-4 * (jumpDistance * jumpDistance - jumpDistance));
+//         this.y = this.ground - height;
+//     }
+//     Entity.prototype.update.call(this);
+// }
+Man.prototype.update = function() {
+    this.x += 2;
+}
+
+
+// Unicorn.prototype.draw = function (ctx) {
+//     if (this.jumping) {
+//         this.jumpAnimation.drawFrame(this.game.clockTick, ctx, this.x + 17, this.y - 34);
+//     }
+//     else {
+//         this.animation.drawFrame(this.game.clockTick, ctx, this.x, this.y);
+//     }
+//     Entity.prototype.draw.call(this);
+// }
+
+Man.prototype.draw = function () {
+    console.log("drawing");
+    this.animation.drawFrame(this.game.clockTick, this.ctx, this.x, this.y);
+}
+
+// the "main" code begins here
+
+var ASSET_MANAGER = new AssetManager();
+
+ASSET_MANAGER.queueDownload("./img/RobotUnicorn.png");
+ASSET_MANAGER.queueDownload("./img/man2.png");
+
+ASSET_MANAGER.downloadAll(function () {
+    console.log("starting up da sheild");
+    var canvas = document.getElementById('gameWorld');
+    var ctx = canvas.getContext('2d');
+
+    var gameEngine = new GameEngine();
+   // var bg = new Background(gameEngine);
+   // var unicorn = new Unicorn(gameEngine);
+
+    //gameEngine.addEntity(bg);
+    //gameEngine.addEntity(unicorn);
+ 
+    gameEngine.init(ctx);
+    gameEngine.start();
+
+    gameEngine.addEntity(new Man(gameEngine, ASSET_MANAGER.getAsset("./img/man2.png")));
+
+});
